584bead1e9a5f9d9d558eb070e1fa7ad0b3c660d
agdales <agdales@yahoo.co.uk>
2024-01-16 23:31:49 +0000

    Added Project Euler Solutions

--- Project Euler/PE_114.py
+++ Project Euler/PE_114.py
@@ -1,14 +1,16 @@
 from functools import lru_cache
 
-# @lru_cache
+
+@lru_cache
 def block_combinations(n):
     if n <= 2:
         combos = 1
     elif n == 3:
         combos = 2
     else:
-        combos = 2 * block_combinations(n-1) + block_combinations(n-4) - block_combinations(n-2)
+        combos = 2 * block_combinations(n - 1) + block_combinations(n - 4) - block_combinations(n - 2)
     return combos
+
 
 num_ways = [block_combinations(i) for i in range(51)]
 print(f'Solution to Project Euler 114 is {num_ways[-1]}')
