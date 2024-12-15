9da49f099da346fd2ecd5e2c937c7dde0d0e0258
agdales <agdales@yahoo.co.uk>
2024-12-15 15:14:55 +0000

    AoC 11b and 12a. Committing before deleting extra code for 12b.

--- Advent of Code/2024/aoc_2024_11.py
+++ Advent of Code/2024/aoc_2024_11.py
@@ -1,9 +1,10 @@
 from functools import lru_cache
+from time import time
 
 test_data = '125 17'
 puzzle_data = '2 72 8949 0 981038 86311 246 7636740'
 
-@lru_cache
+@lru_cache(maxsize=None)
 def split_stones(n):
     if n == 0:
         return (1,)
@@ -14,14 +15,18 @@ def split_stones(n):
     else:
         return (n * 2024, )
 
-@lru_cache
+@lru_cache(maxsize=None)
 def count_splits(n, num_splits=25):
     if num_splits == 0:
         return 1
     else:
         return sum(count_splits(i, num_splits-1) for i in split_stones(n))
 
+tic = time()
 initial_stones = list(map(int, puzzle_data.split(' ')))
 
 print(f'Solution to Day 11a is {sum(count_splits(i) for i in initial_stones)}')
 print(f'Solution to Day 11b is {sum(count_splits(i,75) for i in initial_stones)}')
+toc = time()
+
+print(toc-tic)
